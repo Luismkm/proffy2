7a937bad76ca7efb63a8b26e1c5c6acdca141245
Luismkm <luismarcelo13@yahoo.com.br>
2021-02-20 22:22:26 -0300

    update

--- web/src/assets/components/Select/index.tsx
+++ web/src/assets/components/Select/index.tsx
@@ -5,22 +5,30 @@ import './styles.css'
 interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
     label: string;
     name: string;
-    defaultValue: string;
+    defaultValue?: string;
+    //id?: string;
     options: Array<{
       value: string;
       label: string;
     }>
 }
 
-const Select: React.FC<SelectProps> = ({ label, name, defaultValue, options, ...rest }) => {
+const Select: React.FC<SelectProps> = ({ label, name, defaultValue, id, options, ...rest }) => {
     return(
       <div className="select-block">
         <label htmlFor={ name }>{ label }</label>
         <select id={ name } { ...rest } >
-          <option value="" disabled selected hidden>{ defaultValue }</option>
-          {options.map(option => {
-            return <option key={ option.value } value={ option.value }>{ option.label }</option>
-          })}
+         {
+           defaultValue && <option value="" disabled selected hidden>{ defaultValue }</option>
+           
+         }
+
+         {  
+            options.map(option => {
+                return <option key={ option.value } id={ id } value={ option.value }>{ option.label }</option>
+              }
+            )
+         }
         </select>
       </div>       
     )
